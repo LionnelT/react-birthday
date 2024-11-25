093982415acbb8a509a837c346c76796ac64cb83
LionnelT <lionneltsuro56@gmail.com>
2024-11-25 11:57:02 +0200

    wip

---
+++ src/Card.jsx
@@ -0,0 +1,23 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY Cumi! 🌹</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">HAPPY BIRTHDAY Cumi!</h3>
+        <p>Dear Cumi,</p>
+        <p>
+          Happy birthday! I hope your day is filled with lots of love and
+          laughter! I wish you love, peace and prosperity.
+        </p>
+        {/* <p className="name">Lio</p> */}
+      </div>
+    </div>
+  );
+  
